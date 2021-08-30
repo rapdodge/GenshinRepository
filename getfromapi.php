33d465a2cd5625254be1dc68f3b143b358994aad
Rifqi Arief <33513626+rapdodge@users.noreply.github.com>
2021-08-30 13:00:18 +0700

    Update getfromapi.php
    Revamp

--- getfromapi.php
+++ getfromapi.php
@@ -7,7 +7,6 @@ curl_setopt_array(
 	array(
 		CURLOPT_URL => "https://sdk-os-static.mihoyo.com/hk4e_global/mdk/launcher/api/resource?channel_id=1&key=gcStgarh&launcher_id=10&sub_channel_id=0",
 		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
 		CURLOPT_MAXREDIRS => 10,
 		CURLOPT_TIMEOUT => 30,
 		CURLOPT_FOLLOWLOCATION => true,
@@ -19,6 +18,8 @@ curl_setopt_array(
 $ResponcURL = json_decode(curl_exec($curl), true);
 
 curl_close($curl);
+
+// PEMBATAS
 
 $CekAPI = array();
 
@@ -34,7 +35,9 @@ if ($ResponcURL['message'] != "OK") {
 	$CekAPI['error'] = false;
 	$CekAPI['message'] = 'success';
 
-	$DataTerbaru = array(
+	// PEMBATAS
+
+	$DataGameTerbaru = array(
 		'version' => $ResponcURL['data']['game']['latest']['version'],
 		'path' => $ResponcURL['data']['game']['latest']['path'],
 		'md5' => $ResponcURL['data']['game']['latest']['md5'],
@@ -46,30 +49,75 @@ if ($ResponcURL['message'] != "OK") {
 		$DataSuaraTerbaru['voice_packs'][$k]['name'] = $ResponcURL['data']['game']['latest']['voice_packs'][$k]['name'];
 		$DataSuaraTerbaru['voice_packs'][$k]['path'] = $ResponcURL['data']['game']['latest']['voice_packs'][$k]['path'];
 		$DataSuaraTerbaru['voice_packs'][$k]['md5'] = $ResponcURL['data']['game']['latest']['voice_packs'][$k]['md5'];
-	}
+	};
 
-	$DataTerbaruGabung = array(
-		'latest' => array_merge($DataTerbaru, $DataSuaraTerbaru),
+	$GabungGameSuaraTerbaru = array(
+		'latest' => array_merge($DataGameTerbaru, $DataSuaraTerbaru),
 	);
 
-	$DataPraUnduh = array();
+	$DataUpgradeTerbaru = array();
 	foreach ($ResponcURL['data']['game']['diffs'] as $k0 => $v0) {
-		$DataPraUnduh[$k0]['version'] = $ResponcURL['data']['game']['diffs'][$k0]['version'];
-		$DataPraUnduh[$k0]['name'] = $ResponcURL['data']['game']['diffs'][$k0]['name'];
-		$DataPraUnduh[$k0]['path'] = $ResponcURL['data']['game']['diffs'][$k0]['path'];
-		$DataPraUnduh[$k0]['md5'] = $ResponcURL['data']['game']['diffs'][$k0]['md5'];
+		$DataUpgradeTerbaru[$k0]['version'] = $ResponcURL['data']['game']['diffs'][$k0]['version'];
+		$DataUpgradeTerbaru[$k0]['name'] = $ResponcURL['data']['game']['diffs'][$k0]['name'];
+		$DataUpgradeTerbaru[$k0]['path'] = $ResponcURL['data']['game']['diffs'][$k0]['path'];
+		$DataUpgradeTerbaru[$k0]['md5'] = $ResponcURL['data']['game']['diffs'][$k0]['md5'];
 		foreach ($ResponcURL['data']['game']['diffs'][$k0]['voice_packs'] as $k1 => $v1) {
-			$DataPraUnduh[$k0]['voice_packs'][$k1]['language'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['language'];
-			$DataPraUnduh[$k0]['voice_packs'][$k1]['name'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['name'];
-			$DataPraUnduh[$k0]['voice_packs'][$k1]['path'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['path'];
-			$DataPraUnduh[$k0]['voice_packs'][$k1]['md5'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['md5'];
+			$DataUpgradeTerbaru[$k0]['voice_packs'][$k1]['language'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['language'];
+			$DataUpgradeTerbaru[$k0]['voice_packs'][$k1]['name'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['name'];
+			$DataUpgradeTerbaru[$k0]['voice_packs'][$k1]['path'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['path'];
+			$DataUpgradeTerbaru[$k0]['voice_packs'][$k1]['md5'] = $ResponcURL['data']['game']['diffs'][$k0]['voice_packs'][$k1]['md5'];
 		}
 	}
 
-	$DataPraUnduhGabung = array(
-		'diffs' => $DataPraUnduh,
+	$GabungDataUpgradeTerbaru = array(
+		'diffs' => array_merge($DataUpgradeTerbaru),
 	);
 
-	$Hasil = array_merge($CekAPI, $DataTerbaruGabung, $DataPraUnduhGabung);
+	$GabungDataUpgradeTerbaru = array(
+		'game' => array_merge($GabungGameSuaraTerbaru, $GabungDataUpgradeTerbaru)
+	);
+
+	// PEMBATAS
+
+	$DataPlugin = array();
+	foreach ($ResponcURL['data']['plugin']['plugins'] as $k => $v) {
+		$DataPlugin['plugins'][$k]['name'] = $ResponcURL['data']['plugin']['plugins'][$k]['name'];
+		$DataPlugin['plugins'][$k]['path'] = $ResponcURL['data']['plugin']['plugins'][$k]['path'];
+		$DataPlugin['plugins'][$k]['md5'] = $ResponcURL['data']['plugin']['plugins'][$k]['md5'];
+	}
+
+	$GabungPlugin = array(
+		'plugin' => $DataPlugin
+	);
+
+	// PEMBATAS
+
+	$DataGamePraUnduh = array(
+		'version' => $ResponcURL['data']['pre_download_game']['latest']['version'],
+		'path' => $ResponcURL['data']['pre_download_game']['latest']['path'],
+		'md5' => $ResponcURL['data']['pre_download_game']['latest']['md5'],
+	);
+
+	$DataSuaraPraUnduh = array();
+	foreach ($ResponcURL['data']['pre_download_game']['latest']['voice_packs'] as $k => $v) {
+		$DataSuaraPraUnduh['voice_packs'][$k]['language'] = $ResponcURL['data']['pre_download_game']['latest']['voice_packs'][$k]['language'];
+		$DataSuaraPraUnduh['voice_packs'][$k]['name'] = $ResponcURL['data']['pre_download_game']['latest']['voice_packs'][$k]['name'];
+		$DataSuaraPraUnduh['voice_packs'][$k]['path'] = $ResponcURL['data']['pre_download_game']['latest']['voice_packs'][$k]['path'];
+		$DataSuaraPraUnduh['voice_packs'][$k]['md5'] = $ResponcURL['data']['pre_download_game']['latest']['voice_packs'][$k]['md5'];
+	};
+
+	$GabungGameSuaraPraUnduh = array(
+		'pre_download_game' => array_merge($DataGamePraUnduh, $DataSuaraPraUnduh),
+	);
+
+	// PEMBATAS
+
+	$GabungSemua = array(
+		'data' => array_merge($GabungDataUpgradeTerbaru, $GabungPlugin, $GabungGameSuaraPraUnduh),
+	);
+
+	// PEMBATAS
+
+	$Hasil = array_merge($GabungSemua);
 	print_r(json_encode($Hasil));
 }
